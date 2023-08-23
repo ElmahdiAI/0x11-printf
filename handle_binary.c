d72b75f16ce0614a3d2c26cd8ba57dc0ba5e67a1
ElmahdiAI <mehdiaititto239@gmail.com>
2023-08-22 17:23:29 -0700

    update

---
+++ handle_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * printBinary - Prints the binary representation of an unsigned integer.
+ * @number: The unsigned integer to be printed in binary.
+ * @size: Pointer to track the number of characters printed.
+ * Return: Nothing.
+ */
+void printBinary(unsigned int number, int *size)
+{
+	if (number > 1)
+	{
+		printBinary(number / 2, size);
+	}
+	_putchar('0' + number % 2, size);
+}
