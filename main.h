c404397cbf23405b8454a67ff3cd90aadb276b27
ElmahdiAI <mehdiaititto239@gmail.com>
2023-08-22 17:22:06 -0700

    update

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#define BUFFER_SIZE 1024
+int _putchar(char c, int *size);
+int buffer(char c, int *size);
+int _printf(const char *format, ...);
+void printInteger(int number, int *size);
+void printStr(char *str, int *size);
+void NonPrintableChar(char *str, int *size);
+void printPointer(unsigned int unumber, int *size);
+void handleFormat(char format, va_list args, int *size);
+void printBinary(unsigned int number, int *size);
+void printUnsigned(unsigned int number, int *size);
+void printOctal(unsigned int number, int *size);
+void printHex(unsigned int number, int n, int *size);
+void hexChar(unsigned int hex, int n, int *size);
+#endif /* MAIN_H */
