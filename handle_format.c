9f7fcfee63a92cce1d77744c80c63276dd44c4ab
ElmahdiAI <mehdiaititto239@gmail.com>
2023-08-26 14:51:32 -0700

    update

--- handle_format.c
+++ handle_format.c
@@ -10,15 +10,15 @@
 void handleFormat(char format, void *args, int *size)
 {
 	if (format == 'c')
-		_putchar((int)args, size);
+		_putchar((long int)args, size);
 	else if (format == 's')
 		printStr((char *)args, size);
 	else if (format == '%')
 		_putchar('%', size);
 	else if (format == 'd' || format == 'i')
-		printInteger((int)args, size);
+		printInteger((long int)args, size);
 	else if (format == 'b')
-		printBinary((unsigned int)args, size);
+		printBinary((long int)args, size);
 	else
 		_putchar(format, size);
 }
